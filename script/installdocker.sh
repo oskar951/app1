58a168c1d7d0dda2cc3abc2d46c28f3db0c41bf4
Ubuntu <ubuntu@ip-172-31-41-250.eu-west-1.compute.internal>
2021-01-13 16:38:41 +0000

    file to test git push

--- script/installdocker.sh
+++ script/installdocker.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y  apt-transport-https ca-certificates curl software-properties-common
 
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -13,7 +13,7 @@ sudo apt update
 
 apt-cache policy docker-ce
 
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 
 sudo systemctl status docker
 
